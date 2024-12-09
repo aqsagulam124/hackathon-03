c011fc3fdf7bd036636b25dc9aeb944d999a50e8
Aqsa <aqsagulamhasan@gmail.com>
2024-12-09 22:36:11 +0500

    Add files via upload

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import AboutPage from "@/components/about-header"
+import React from 'react'
+
+const About = () => {
+  return (
+    <>
+    <AboutPage />
+    </>
+  )
+}
+
+export default About
